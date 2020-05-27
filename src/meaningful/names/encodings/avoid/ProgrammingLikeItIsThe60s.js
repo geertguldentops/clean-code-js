e07d86c8a26a6b916fc920a5731470963aa6b1d4
Geert Guldentops <geert.guldentops@corsearch.com>
2020-05-27 08:03:37 +0200

    Improve "ProgrammingLikeItIsThe60s" demo

--- src/meaningful/names/encodings/avoid/ProgrammingLikeItIsThe60s.js
+++ src/meaningful/names/encodings/avoid/ProgrammingLikeItIsThe60s.js
@@ -1,11 +1,11 @@
 module.exports = class ReportDefinitionModel {
 
     // Avoid this! Encodings make the code unnecessary difficult to read!
-    constructor(m_int_id, m_str_name, m_str_category, m_str_locationType, m_rec_periodSelection) {
-        this.m_int_id = m_int_id;
-        this.m_str_name = m_str_name;
-        this.m_str_category = m_str_category;
-        this.m_str_locationType = m_str_locationType;
-        this.m_rec_periodSelection = m_rec_periodSelection;
+    constructor(int_id, str_name, str_category, str_locationType, rec_periodSelection) {
+        this.m_int_id = int_id;
+        this.m_str_name = str_name;
+        this.m_str_category = str_category;
+        this.m_str_locationType = str_locationType;
+        this.m_rec_periodSelection = rec_periodSelection;
     }
 };
